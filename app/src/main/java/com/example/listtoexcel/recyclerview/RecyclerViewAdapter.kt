55134285414222b22b2e01d15b32df4ae504f2a3
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-09-30 19:47:44 +0300

    #13 dependecy added. ViewHolder and RecyclerViewAdapter are writen.

--- app/src/main/java/com/example/listtoexcel/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/listtoexcel/recyclerview/RecyclerViewAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.listtoexcel.recyclerview
 
-class RecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listtoexcel.R
+import com.example.listtoexcel.add.WorkDetails
+import kotlinx.android.synthetic.main.list_item_layout.view.*
+
+class RecyclerViewAdapter(var workDetails: List<WorkDetails>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.apply {
+            tvDate.text = workDetails[position].date
+            tvDailyJob.text = workDetails[position].dailyJob
+            tvPrice.text = workDetails[position].price.toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return workDetails.size
+    }
 }
