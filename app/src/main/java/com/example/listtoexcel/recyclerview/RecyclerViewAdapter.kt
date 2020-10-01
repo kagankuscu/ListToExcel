864bfa8e96f34998f1b9f1d08e364319fadae08d
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-10-01 18:24:04 +0300

    #17 click event listener added.

--- app/src/main/java/com/example/listtoexcel/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/listtoexcel/recyclerview/RecyclerViewAdapter.kt
@@ -3,6 +3,7 @@ package com.example.listtoexcel.recyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listtoexcel.R
 import com.example.listtoexcel.add.WorkDetails
@@ -11,7 +12,27 @@ import kotlinx.android.synthetic.main.list_item_layout.view.*
 class RecyclerViewAdapter(var workDetails: ArrayList<WorkDetails>) :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { v: View ->
+
+                // i will change it. i am going to navigate different page user can see all info about it.
+
+                Toast.makeText(
+                    itemView.context,
+                    itemView.context.resources.getString(
+                        R.string.toast_message,
+                        workDetails[adapterPosition].date,
+                        workDetails[adapterPosition].dailyJob,
+                        workDetails[adapterPosition].workers,
+                        workDetails[adapterPosition].price.toString(),
+                        workDetails[adapterPosition].totalPrice.toString()
+                    ),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
