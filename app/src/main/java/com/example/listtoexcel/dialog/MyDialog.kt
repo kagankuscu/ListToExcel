871b3cf266eb4bec89facc657697122ad3b737a9
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-10-03 18:00:42 +0300

    #19

--- app/src/main/java/com/example/listtoexcel/dialog/MyDialog.kt
+++ app/src/main/java/com/example/listtoexcel/dialog/MyDialog.kt
@@ -1,4 +1,22 @@
 package com.example.listtoexcel.dialog
 
-class MyDialog {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDialogFragment
+
+class MyDialog : AppCompatDialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alertDialog = AlertDialog.Builder(activity)
+            .setTitle("Information")
+            .setMessage("Date")
+            .setMessage("Daily Job")
+            .setPositiveButton("Ok", DialogInterface.OnClickListener { _, _ ->
+
+            }).create()
+
+        return alertDialog
+    }
 }
